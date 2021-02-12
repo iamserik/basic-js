07bc7a00387face9cc5d65d7d3032c8b9533b7a2
Serik <serj_navii@mail.ru>
2021-02-12 06:23:44 +0600

    feat: solve problems extended-repeater and hanoi-tower

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,25 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+  let res = [];
+  let ad = [];
+  let str = arguments[0];
+  if(typeof str !== 'string') str += '';
+  const options = arguments[1];
+  let repeatTimes = options.repeatTimes === undefined ? 1 : options.repeatTimes;
+  let separator = options.separator === undefined ? '+' : options.separator;
+  let addition = options.addition !== undefined ? options.addition + '' : options.addition;
+  let additionRepeatTimes = options.additionRepeatTimes === undefined ? 1 : options.additionRepeatTimes;
+  let additionSeparator = options.additionSeparator === undefined ? '|' : options.additionSeparator;
+
+  for(let j = 0; j < additionRepeatTimes; j++){
+    ad.push(addition);
+  }
+
+  for(let i = 0; i < repeatTimes; i++){
+    let strWithAddit = str.concat((ad.length !== 0) ? ad.join(additionSeparator) : '');
+    res.push(strWithAddit);
+  }
+  return res.join(separator);
 };
   
--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,14 +1,11 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+  let turns = (2**arguments[0])-1;
+  let seconds = Math.floor((3600/arguments[1])*turns);
 
-  const disksNumber = arguments[0];
-  const turnsSpeed = arguments[1];
-  let rods = [[], [], []];
-  let turns = 0;
-  let seconds = 0;
-
-  for(let i = disksNumber; i <= 1; i--) rod[0].push(i);
+  return {
+    turns: turns,
+    seconds: seconds
+  }
 };
